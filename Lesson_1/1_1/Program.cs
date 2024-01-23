f578ebd27d26c7340f04e5d18a5a06c7a4f8a605
Darina Romashova <dariro.china@gmail.com>
2024-01-23 23:14:46 +0700

    Lesson1 решили первую задачку на SHARP

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// Напишите программу, которая на вход принимает два числа
+// и проверяет, является ли одно число квадратом второго
+
+Console.Write("Enter the first number: ");
+int b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the second number: ");
+int b2 = int.Parse(Console.ReadLine()!);
+if (b1 * b1 == b2) 
+    Console.WriteLine("Yes");
+else 
+    Console.WriteLine("No"); 
+    // Первая задача выполнена
