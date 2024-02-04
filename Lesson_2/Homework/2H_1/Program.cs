150ee28d5c6b097e9f60cfee5be21b19669e87b2
Darina Romashova <dariro.china@gmail.com>
2024-02-04 20:54:40 +0700

    Lesson 2 Homework 2_1 Решили первую задачу

---
+++ Lesson_2/Homework/2H_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+//23 и 7
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+if (n % 7 == 0 && n % 23 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
