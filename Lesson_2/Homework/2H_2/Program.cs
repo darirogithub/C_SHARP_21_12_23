58f51f618e5b87f773f58a62dd93298a16216618
Darina Romashova <dariro.china@gmail.com>
2024-02-04 21:25:16 +0700

    Lesson 2 Homework 2_2 Решили вторую задачу

---
+++ Lesson_2/Homework/2H_2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+Console.WriteLine("Введите первое число: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int y = int.Parse(Console.ReadLine()!);
+
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("Точка находится в четверти N2");
+}
+else
+{
+    if (x < 0 && y < 0)
+    {
+        Console.WriteLine("Точка находится в четверти N3");
+    }
+    else
+    {
+        if (x > 0 && y < 0)
+        {
+            Console.WriteLine("Точка находится в четверти N4");
+        }
+        else
+        {
+            if (x > 0 && y > 0)
+                Console.WriteLine("Точка находится в четверти N1");
+        }
+    }
+}
