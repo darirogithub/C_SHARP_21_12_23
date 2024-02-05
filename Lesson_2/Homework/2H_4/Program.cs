dc7905abdef308228f8d34afd3197e33ac1bc602
Darina Romashova <dariro.china@gmail.com>
2024-02-05 21:44:44 +0700

    Списали последнюю задачу

---
+++ Lesson_2/Homework/2H_4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+
+string MaxNum(int num)
+{
+Console.WriteLine(num);
+string allNums = "";
+
+while (num > 0)
+{
+    allNums = num % 10 + "," + allNums;
+    num /=10;
+}
+return allNums;
+}
+
+Console.WriteLine(MaxNum(new Random().Next(1, 100000)));
+// Задача не решена, не помню, как работать с массивами. 
